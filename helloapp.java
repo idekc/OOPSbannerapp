a4e6cb95d819fa437379c2304b3e799e5b5eb533
atmanjain <atman.jain12819@gmail.com>
2026-03-11 14:34:11 +0530

    UC3 HelloApp

--- helloapp.java
+++ helloapp.java
@@ -2,6 +2,10 @@ public class helloapp {
 
     public static void main(String[] args) {
         String name = "ATMAN";
+
+        if(args.length>0){
+            name = args[0];
+        }
         System.out.println("Hello, " + name + "!");
     }
 
