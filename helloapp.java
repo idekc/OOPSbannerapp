710cf2b5f8a0e111a70d458e680a27f296290b08
atmanjain <atman.jain12819@gmail.com>
2026-04-13 12:37:05 +0530

    Initial commit

--- helloapp.java
+++ helloapp.java
@@ -1,12 +1,16 @@
+
 public class helloapp {
 
     public static void main(String[] args) {
-        String name = "ATMAN";
 
-        if(args.length>0){
-            name = args[0];
+        if (args.length > 0) {
+            // Join all arguments with space
+            String name = String.join(" ", args);
+            System.out.println("Hello " + name);
+        } else {
+            System.out.println("Hello User");
         }
-        System.out.println("Hello, " + name + "!");
+
     }
 
 }
